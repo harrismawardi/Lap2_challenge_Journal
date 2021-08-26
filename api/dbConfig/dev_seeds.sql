f7d866bd0cea867d535153a6ebb4df7f494e72c4
Simon Allan <stallan464@googlemail.com>
2021-08-26 12:16:13 +0100

    corrected syntax error in dev_seeds.sql

--- api/dbConfig/dev_seeds.sql
+++ api/dbConfig/dev_seeds.sql
@@ -1,6 +1,17 @@
 INSERT INTO posts (author_name, title, story)
-VALUES (
-    ('Silky Badger', 'life is sad', 'no body compliments me on how silky my fur is'),
-    ('joyous pigeon', 'I am dirty but happy', 'people think I am gross, but I do not care')
-    ('confident rat', 'life is good and i am great', 'get out the way - h8rs gonna h8!')
+VALUES
+(
+    'Silky Badger', 
+    'life is sad', 
+    'no body compliments me on how silky my fur is'
+),
+(
+    'joyous pigeon', 
+    'I am dirty but happy', 
+    'people think I am gross, but I do not care'
+),
+(
+    'confident rat', 
+    'life is good and i am great', 
+    'get out the way - h8rs gonna h8!'
 );
